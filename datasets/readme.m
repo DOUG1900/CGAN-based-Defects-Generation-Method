272944be9db36c0cf5866c29abf4c7a9e1c3ed51
DOUG1900 <85465471+DOUG1900@users.noreply.github.com>
2022-04-13 14:43:23 +0800

    Update readme.m

--- datasets/readme.m
+++ datasets/readme.m
@@ -1,3 +1,3 @@
-Unzip the two files, you can get the data.
-The dataset_h and dataset_s respectively refer to swivel clevis pins in horizontal and vertical directions.
-Do not forget to change the path of data in the training program.
+# Unzip the two files, you can get the data.
+# The dataset_h and dataset_s respectively refer to swivel clevis pins in horizontal and vertical directions.
+# Do not forget to change the path of data in the training program.
