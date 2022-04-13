979d4655701b94bee156f8599aa4f53e11dad7e0
DOUG1900 <85465471+DOUG1900@users.noreply.github.com>
2022-04-13 13:55:25 +0800

    Update readme.m

--- datasets/readme.m
+++ datasets/readme.m
@@ -1,2 +1,2 @@
-### Unzip the two files, you can get the data.
-### The dataset_h and dataset_s respectively refer to swivel clevis pins in horizontal and vertical directions.
+Unzip the two files, you can get the data.
+The dataset_h and dataset_s respectively refer to swivel clevis pins in horizontal and vertical directions.
